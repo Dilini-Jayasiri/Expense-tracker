df3e18a05ef4f4c988511cffeda574fdf4490f59
Dilini Jayasiri <dilininadeesha2018@gmail.com>
2024-03-22 22:39:32 +0530

    Transaction added

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -9,13 +9,34 @@ namespace Expense_tracker.Models
         [Key]
         public int TransactionId { get; set; }
 
+        [Range(1, int.MaxValue,ErrorMessage ="Please select a category.")]
         public int CategoryId { get; set; }
-        public Category Category{ get; set; } 
+        public Category? Category { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be grater than zero.")]
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
 
         public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+
+        }
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Title == "Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+
+        }
     }
 }
